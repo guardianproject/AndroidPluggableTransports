d5912f822cf3b867feb13cc4c89c660b90e46d8e
n8fr8 <nathan@freitas.net>
2017-10-18 13:54:27 -0400

    add ability to reply via broadcastevent

--- APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchConstants.java
+++ APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchConstants.java
@@ -1,11 +1,54 @@
 package info.pluggabletransports.aptds;
 
+import android.content.Intent;
+
 /**
  * Created by n8fr8 on 10/18/17.
  */
 
 public interface DispatchConstants {
 
-    public final static String ACTION_START = "info.pluggabletransports.intent.action.START";
-    public final static String EXTRA_PACKAGE_NAME = "info.pluggabletransports.aptds";
+    /**
+     * A request to Orbot to transparently start Tor services
+     */
+    public final static String ACTION_START = "info.pluggabletransports.aptds.intent.action.START";
+    /**
+     * {@link Intent} send by Orbot with {@code ON/OFF/STARTING/STOPPING} status
+     */
+    public final static String ACTION_STATUS = "info.pluggabletransports.aptds.intent.action.STATUS";
+    /**
+     * {@code String} that contains a status constant: {@link #STATUS_ON},
+     * {@link #STATUS_OFF}, {@link #STATUS_STARTING}, or
+     * {@link #STATUS_STOPPING}
+     */
+    public final static String EXTRA_STATUS = "info.pluggabletransports.aptds.intent.extra.STATUS";
+    /**
+     * A {@link String} {@code packageName} for Orbot to direct its status reply
+     * to, used in {@link #ACTION_START} {@link Intent}s sent to Orbot
+     */
+    public final static String EXTRA_PACKAGE_NAME = "info.pluggabletransports.aptds.intent.extra.PACKAGE_NAME";
+    /**
+     * The SOCKS proxy settings in URL form.
+     */
+    public final static String EXTRA_SOCKS_PROXY = "info.pluggabletransports.aptds.intent.extra.SOCKS_PROXY";
+    public final static String EXTRA_SOCKS_PROXY_HOST = "info.pluggabletransports.aptds.intent.extra.SOCKS_PROXY_HOST";
+    public final static String EXTRA_SOCKS_PROXY_PORT = "info.pluggabletransports.aptds.intent.extra.SOCKS_PROXY_PORT";
+    /**
+     * The HTTP proxy settings in URL form.
+     */
+    public final static String EXTRA_HTTP_PROXY = "info.pluggabletransports.aptds.intent.extra.HTTP_PROXY";
+    public final static String EXTRA_HTTP_PROXY_HOST = "info.pluggabletransports.aptds.intent.extra.HTTP_PROXY_HOST";
+    public final static String EXTRA_HTTP_PROXY_PORT = "info.pluggabletransports.aptds.intent.extra.HTTP_PROXY_PORT";
+
+    /**
+     * All tor-related services and daemons are stopped
+     */
+    public final static String STATUS_OFF = "OFF";
+    /**
+     * All tor-related services and daemons have completed starting
+     */
+    public final static String STATUS_ON = "ON";
+    public final static String STATUS_STARTING = "STARTING";
+    public final static String STATUS_STOPPING = "STOPPING";
+
 }
--- APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchService.java
+++ APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchService.java
@@ -5,12 +5,13 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 
 /**
  * Created by n8fr8 on 10/18/17.
  */
 
-public class DispatchService extends Service {
+public class DispatchService extends Service implements DispatchConstants {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -30,5 +31,42 @@ public class DispatchService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    /**
+     * Send Orbot's status in reply to an
+     * {@link DispatchConstants#ACTION_START} {@link Intent}, targeted only to
+     * the app that sent the initial request. If the user has disabled auto-
+     * starts, the reply {@code ACTION_START Intent} will include the extra
+     */
+    private void replyWithStatus(Intent startRequest, int status, int socksPort, int httpPort) {
+        String packageName = startRequest.getStringExtra(EXTRA_PACKAGE_NAME);
+
+        Intent reply = new Intent(ACTION_STATUS);
+        reply.putExtra(EXTRA_STATUS, status);
+
+        if (socksPort != -1) {
+            reply.putExtra(EXTRA_SOCKS_PROXY, "socks://127.0.0.1:" + socksPort);
+            reply.putExtra(EXTRA_SOCKS_PROXY_HOST, "127.0.0.1");
+            reply.putExtra(EXTRA_SOCKS_PROXY_PORT, socksPort);
+        }
+
+        if (httpPort != -1) {
+            reply.putExtra(EXTRA_HTTP_PROXY, "http://127.0.0.1" + httpPort);
+            reply.putExtra(EXTRA_HTTP_PROXY_HOST, "127.0.0.1");
+            reply.putExtra(EXTRA_HTTP_PROXY_PORT, httpPort);
+        }
+
+        if (packageName != null)
+        {
+            reply.setPackage(packageName);
+            sendBroadcast(reply);
+        }
+        else
+        {
+            LocalBroadcastManager.getInstance(this).sendBroadcast(reply);
+
+        }
+
     }
 }
