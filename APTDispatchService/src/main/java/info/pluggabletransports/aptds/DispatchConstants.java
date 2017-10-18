075965d6caf5c29dfaf8f1244c4c0707925178fe
n8fr8 <nathan@freitas.net>
2017-10-18 12:18:01 -0400

    update intent start action string

--- APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchConstants.java
+++ APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchConstants.java
@@ -6,6 +6,6 @@ package info.pluggabletransports.aptds;
 
 public interface DispatchConstants {
 
-    public final static String ACTION_START = "aptds_start";
+    public final static String ACTION_START = "info.pluggabletransports.intent.action.START";
     public final static String EXTRA_PACKAGE_NAME = "info.pluggabletransports.aptds";
 }
