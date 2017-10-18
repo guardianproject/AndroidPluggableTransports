27f7a70162040756059258d5043a8143f38f881e
n8fr8 <nathan@freitas.net>
2017-10-18 12:24:20 -0400

    add service definitions in classes and manifest

--- APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchService.java
+++ APTDispatchService/src/main/java/info/pluggabletransports/aptds/DispatchService.java
@@ -1,8 +1,34 @@
 package info.pluggabletransports.aptds;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+
 /**
  * Created by n8fr8 on 10/18/17.
  */
 
-public class DispatchService {
+public class DispatchService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
---
+++ APTDispatchService/src/main/java/info/pluggabletransports/aptds/vpn/DispatchVPN.java
@@ -0,0 +1,10 @@
+package info.pluggabletransports.aptds.vpn;
+
+import android.net.VpnService;
+
+/**
+ * Created by n8fr8 on 10/18/17.
+ */
+
+public class DispatchVPN extends VpnService {
+}
