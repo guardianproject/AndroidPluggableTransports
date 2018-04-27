b39e4c2d904bb18aecb429a02f7e9b63293aa943
n8fr8 <nathan@freitas.net>
2018-04-27 12:05:26 -0400

    handle null connections

--- sample/src/main/java/info/pluggeabletransports/sample/SampleClientActivity.java
+++ sample/src/main/java/info/pluggeabletransports/sample/SampleClientActivity.java
@@ -1,5 +1,6 @@
 package info.pluggeabletransports.sample;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -21,8 +22,22 @@ public class SampleClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_client);
 
-        initMeekTransport();
-        initSampleTransport();
+        // The Very Basic
+        new AsyncTask<Void, Void, Void>() {
+            protected void onPreExecute() {
+                // Pre Code
+            }
+            protected Void doInBackground(Void... unused) {
+                // Background Code
+                initMeekTransport();
+                initSampleTransport();
+                return null;
+            }
+            protected void onPostExecute(Void unused) {
+                // Post Code
+            }
+        }.execute();
+
     }
 
     private void initMeekTransport ()
@@ -46,24 +61,25 @@ public class SampleClientActivity extends AppCompatActivity {
 
         Connection conn = init ("sample",bridgeAddress, options);
 
-        //now use the connection, either as a proxy, or to read and write bytes directly
-        if (conn.getLocalAddress() != null && conn.getLocalPort() != -1)
-            setSocksProxy (conn.getLocalAddress(), conn.getLocalPort());
-        else
-        {
+        if (conn != null) {
+            //now use the connection, either as a proxy, or to read and write bytes directly
+            if (conn.getLocalAddress() != null && conn.getLocalPort() != -1)
+                setSocksProxy(conn.getLocalAddress(), conn.getLocalPort());
+            else {
 
-            //or read and write bytes directly!
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try {
-                baos.write("GET https://somewebsite.org/TheProject.html HTTP/1.0".getBytes());
-                conn.write(baos.toByteArray());
+                //or read and write bytes directly!
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                try {
+                    baos.write("GET https://somewebsite.org/TheProject.html HTTP/1.0".getBytes());
+                    conn.write(baos.toByteArray());
 
-                byte[] buffer = new byte[1024*64];
-                int read = conn.read(buffer,0,buffer.length);
-                String response = new String(buffer);
+                    byte[] buffer = new byte[1024 * 64];
+                    int read = conn.read(buffer, 0, buffer.length);
+                    String response = new String(buffer);
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
