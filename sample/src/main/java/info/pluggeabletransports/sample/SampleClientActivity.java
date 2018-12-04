a4cf541fbae2c1b97edfd1e086ba614cb404c184
n8fr8 <nathan@guardianproject.info>
2018-12-04 14:29:21 -0500

    update sample to work with latest updates

--- sample/src/main/java/info/pluggeabletransports/sample/SampleClientActivity.java
+++ sample/src/main/java/info/pluggeabletransports/sample/SampleClientActivity.java
@@ -3,10 +3,13 @@ package info.pluggeabletransports.sample;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Collections;
 import java.util.Properties;
 
 import info.pluggabletransports.dispatch.Connection;
@@ -15,6 +18,10 @@ import info.pluggabletransports.dispatch.Dispatcher;
 import info.pluggabletransports.dispatch.Transport;
 import info.pluggabletransports.dispatch.transports.MeekTransport;
 import info.pluggabletransports.dispatch.transports.sample.SampleTransport;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class SampleClientActivity extends AppCompatActivity {
 
@@ -24,37 +31,89 @@ public class SampleClientActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sample_client);
 
         // The Very Basic
-        new AsyncTask<Void, Void, Void>() {
+        new AsyncTask<Void, Void, String>() {
             protected void onPreExecute() {
                 // Pre Code
             }
 
-            protected Void doInBackground(Void... unused) {
+            protected String doInBackground(Void... unused) {
                 // Background Code
                 initMeekTransport();
-                initSampleTransport();
+             //   initSampleTransport();
                 return null;
             }
 
-            protected void onPostExecute(Void unused) {
+            protected void onPostExecute(String log) {
                 // Post Code
+                if (log != null)
+                    ((TextView)findViewById(R.id.response)).setText(log);
+
             }
         }.execute();
 
     }
 
-    private void initMeekTransport() {
+    private String initMeekTransport() {
         new MeekTransport().register();
 
         Properties options = new Properties();
-        String remoteAddress = "185.152.65.180:9001";// a public Tor guard to test
+        String remoteAddress = "en.wikipedia.org:443";// a public Tor guard to test
 
-        options.put(MeekTransport.OPTION_URL,"https://meek.azureedge.net/"); //the public Tor Meek endpoint
-        options.put(MeekTransport.OPTION_FRONT, "ajax.aspnetcdn.com"); //the domain fronting address to use
-        options.put(MeekTransport.OPTION_KEY, "97700DFE9F483596DDA6264C4D7DF7641E1E39CE"); //the key that is needed for this endpoint
+        options.put(MeekTransport.OPTION_URL,"https://myprivatemeek.azureedge.net/"); //the public Tor Meek endpoint
+        options.put(MeekTransport.OPTION_FRONT, "ajax.aspnetcdn.com"); //the domain fronting address to use for Azure
+        options.put(MeekTransport.OPTION_KEY, "88880DFE9F483596DDA6264C4D7DF7641E1E39CE"); //the unique meek key that is needed for this endpoint
 
-        init(DispatchConstants.PT_TRANSPORTS_MEEK, remoteAddress, options);
+        Connection conn = init(DispatchConstants.PT_TRANSPORTS_MEEK, remoteAddress, options);
 
+        if (conn != null) {
+            //now use the connection, either as a proxy, or to read and write bytes directly
+            if (conn.getLocalAddress() != null && conn.getLocalPort() != -1) {
+
+                System.setProperty("socksProxyHost", conn.getLocalAddress().getHostAddress());
+                System.setProperty("socksProxyPort", conn.getLocalPort()+"");
+
+                OkHttpClient client = new OkHttpClient();
+
+                // Create request for remote resource
+                Request request = new Request.Builder()
+                        .url("https://" + remoteAddress)
+                        .build();
+
+                // Execute the request and retrieve the response.
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+
+                    // Deserialize HTTP response to concrete type.
+                    ResponseBody body = response.body();
+
+                    Log.d("Sample","Got page via Meek: " + body);
+
+                    return body.toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else {
+
+                //or read and write bytes directly!
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                try {
+                    baos.write("GET https://somewebsite.org/TheProject.html HTTP/1.0".getBytes());
+                    conn.write(baos.toByteArray());
+
+                    byte[] buffer = new byte[1024 * 64];
+                    int read = conn.read(buffer, 0, buffer.length);
+                    String response = new String(buffer);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
     }
 
     private void initSampleTransport() {
@@ -89,11 +148,11 @@ public class SampleClientActivity extends AppCompatActivity {
         }
     }
 
-    public Connection init(String type, String bridgeAddress, Properties options) {
+    public Connection init(String type, String remoteAddress, Properties options) {
         Transport transport = Dispatcher.get().getTransport(this, type, options);
 
         if (transport != null) {
-            Connection conn = transport.connect(bridgeAddress);
+            Connection conn = transport.connect(remoteAddress);
 
             return conn;
 
